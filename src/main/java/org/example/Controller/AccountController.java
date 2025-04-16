09faac3a573e672d71d04978254847332f865aae
Aryan Patel <patel.aryan9511@gmail.com>
2025-04-16 01:43:28 -0400

    new update

--- src/main/java/org/example/Controller/AccountController.java
+++ src/main/java/org/example/Controller/AccountController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/accounts2")
+@RequestMapping("/api/accounts")
 public class AccountController {
 
     @Autowired
